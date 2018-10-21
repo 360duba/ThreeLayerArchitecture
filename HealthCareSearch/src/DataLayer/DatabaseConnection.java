10b6bc8e254e2c784231a3d51cc8c31de5f67b4f
IAmSmantha <44025049+IAmSmantha@users.noreply.github.com>
2018-10-21 19:04:07 +0800

    Update DatabaseConnection.java

--- HealthCareSearch/src/DataLayer/DatabaseConnection.java
+++ HealthCareSearch/src/DataLayer/DatabaseConnection.java
@@ -25,7 +25,7 @@ public class DatabaseConnection {
 	private static Connection CreateNewConnection() throws SQLException {
 		try {
 			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-			conn = DriverManager.getConnection("jdbc:sqlserver://localhost:1433;DatabaseName=HealthCare", username, password);
+			conn = DriverManager.getConnection("jdbc:sqlserver://rm-bp1ln3783824nh98t0o.sqlserver.rds.aliyuncs.com:3433;DatabaseName=Healthcare", username, password);
 		} catch (Exception e) {
             JOptionPane.showMessageDialog(null, "账号密码错误","消息提示",JOptionPane.ERROR_MESSAGE);
             e.printStackTrace();
